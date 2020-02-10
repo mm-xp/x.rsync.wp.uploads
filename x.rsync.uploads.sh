38db988d708ab323216e71cd538af407a79fdaf5
Xopher <xpollard@maddenmedia.com>
2020-02-10 16:39:07 -0700

    :shirt: cleanup

--- x.rsync.uploads.sh
+++ x.rsync.uploads.sh
@@ -11,6 +11,6 @@ done
 
 # KEYS & PATHS
 KEY_AWS_MM="~/.ssh/mm-tuc-dt-8200-xp-key.pem"
-UPLOADS_DIR="/var/www/$DOMAIN/public/content/uploads"
+UPLOADS_DIR="/var/www/$DOMAIN/public/content/uploads/"
 
-rsync -r -a -v -e "ssh -i $KEY_AWS_MM" ubuntu@$INSTANCE:$UPLOADS_DIR/ $UPLOADS_DIR/
+sudo rsync -r -a -v -e "ssh -i $KEY_AWS_MM" ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR
