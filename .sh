7c85cb361c5fa692fc19c74b65240200a7e9d3cd
Xopher <xpollard@maddenmedia.com>
2020-02-11 12:03:52 -0700

    :recycle: Refactor code

--- .sh
+++ .sh
@@ -13,6 +13,8 @@ done
 KEY_AWS_MM="/home/ubuntu/.ssh/mm-tuc-dt-8200-xp-key.pem"
 UPLOADS_DIR="/var/www/$DOMAIN/public/content/uploads/"
 
-sudo runuser -l root -c "ssh-keyscan  $INSTANCE >> ~/.ssh/known_hosts"
-sudo runuser -l root -c "sudo rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR"
-sudo runuser -l root -c "ssh-keygen -R $INSTANCE"
+CMD1="ssh-keyscan  $INSTANCE >> ~/.ssh/known_hosts"
+CMD2="sudo rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR"
+CMD3="ssh-keygen -R $INSTANCE"
+
+sudo runuser -l root -c "${CMD1}; ${CMD2}; ${CMD3};" 
