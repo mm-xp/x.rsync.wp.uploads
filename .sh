1c2cc8e25f40188b5ac1dfd9cc7e86eda4bd5c18
Xopher <xpollard@maddenmedia.com>
2020-02-11 11:44:14 -0700

    :lock: Add instances to knownhosts before connecting and remove once complete

--- .sh
+++ .sh
@@ -13,5 +13,6 @@ done
 KEY_AWS_MM="/home/ubuntu/.ssh/mm-tuc-dt-8200-xp-key.pem"
 UPLOADS_DIR="/var/www/$DOMAIN/public/content/uploads/"
 
-sudo ssh-keyscan  $INSTANCE >> ~/.ssh/known_hosts
+sudo runuser -l root -c "ssh-keyscan  $INSTANCE >> ~/.ssh/known_hosts"
 sudo runuser -l root -c "sudo rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR"
+sudo runuser -l root -c "ssh-keygen -R $INSTANCE"
