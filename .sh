594e2ac06d4dd8059520c16d3599d641f3bd34c6
Xopher <xpollard@maddenemedia.com>
2020-09-23 22:36:58 -0700

    🚀 rm sudo

--- .sh
+++ .sh
@@ -14,8 +14,8 @@ KEY_AWS_MM="/home/ubuntu/.ssh/mm-tuc-dt-8200-xp-key.pem"
 UPLOADS_DIR="/var/www/$DOMAIN/public/content/uploads/"
 
 _KEYSCAN="ssh-keyscan  $INSTANCE >> ~/.ssh/known_hosts"
-_RSYNCUP="sudo rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR"
-_RSYNCDN="sudo rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' $UPLOADS_DIR ubuntu@$INSTANCE:$UPLOADS_DIR"
+_RSYNCUP="rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' ubuntu@$INSTANCE:$UPLOADS_DIR $UPLOADS_DIR"
+_RSYNCDN="rsync -r -a -v -e 'ssh -i $KEY_AWS_MM' $UPLOADS_DIR ubuntu@$INSTANCE:$UPLOADS_DIR"
 _KEYGEN="ssh-keygen -R $INSTANCE"
 
 sudo runuser -l root -c "${_KEYSCAN}; ${_RSYNCUP}; ${_RSYNCDN}; ${_KEYGEN};" 
